3e5bc2e850c3c82f93bf926a67c0a5a1b81b39ca
Abhilash Gaur <abhigaur100@gmail.com>
2022-07-19 16:37:31 +0530

    commit array program

---
+++ src/main/java/arraysPrograms/TestArrayProgram.java
@@ -0,0 +1,12 @@
+package arraysPrograms;
+
+public class TestArrayProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test");
+
+	}
+
+}
